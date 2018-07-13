d4cc18c757ff5bea3ca1e247cbe0c29d677dd0b7
Pareder <ruskc2beat@gmail.com>
2018-07-13 20:33:50 +0300

    Add new functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const compression = require('compression')
 
 app = express()
 app.use(serveStatic(__dirname + "/dist"))
+app.use(compression())
 
 const port = process.env.PORT || 5000
 
