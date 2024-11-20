bc9ab71ca5e4d19130f98852d3797d74f49685e5
Pareder <ruskc2beat@gmail.com>
2024-11-20 11:00:15 +0200

    Revert "Update Node version, remove server file and dependencies"
    This reverts commit c7d293f86b3fc1fa8421d91e8cd917120eaee7d6.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const compression = require('compression')
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'), {
+  maxAge: 604800000,
+  setHeaders: function (res) {
+    res.setHeader('X-FRAME-OPTIONS', 'DENY')
+    res.setHeader('X-XSS-Protection', '1; mode=block')
+    res.setHeader('X-Content-Type-Options', 'nosniff')
+    res.setHeader('Cache-Control', 'max-age=604800000')
+    res.setHeader('Expires', new Date(Date.now() + 2592000000 * 30).toUTCString())
+  }
+})
+
+const app = express()
+app.use(compression())
+app.use(staticFileMiddleware)
+
+const port = process.env.PORT || 5000
+
+app.listen(port)
